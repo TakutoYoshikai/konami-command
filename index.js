63781f808649b6c57d9434aedcf1a50d9bfc6e44
lucky <takuto.yoshikai@gmail.com>
2021-05-03 14:25:44 +0900

    init

---
+++ index.js
@@ -0,0 +1,29 @@
+
+function setKonamiCommand(callback) {
+  const konamiCommand = [
+    38,
+    38,
+    40,
+    40,
+    37,
+    39,
+    37,
+    39,
+    66,
+    65,
+  ];
+  let currentIndex = 0;
+  document.addEventListener("keydown", (event) => {
+    if (!event) {
+      return;
+    }
+    if (event.keyCode === konamiCommand[currentIndex]) {
+      currentIndex++;
+    } else {
+      currentIndex = 0;
+    }
+    if (currentIndex === konamiCommand.length) {
+      callback();
+    }
+  });
+}
